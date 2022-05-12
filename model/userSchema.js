2cf01e62df95b6ec9c62b133f2d0fcb00f0f19d3
syedshaneraza <syed.shaneraza@phaedrasolutions.com>
2022-05-12 12:27:58 +0500

    15 - bcrypt + jwt implemented

--- model/userSchema.js
+++ model/userSchema.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const bcrypt = require('bcryptjs');
+const jwt = require("jsonwebtoken");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -22,17 +23,38 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  tokens: [
+    {
+      token: {
+        type: String,
+        required: true
+      }
+    }
+  ]
 });
 
 // hashing the password using bcryptjs
 userSchema.pre('save', async function (next) {
-  console.log('here')
   if(this.isModified('password')) {
     this.password = await bcrypt.hash(this.password, 12);
     this.cpassword = await bcrypt.hash(this.cpassword, 12);
   }
   next();
 });
+
+// generating auth token
+userSchema.methods.generateAuthToken = async function () {
+  try {
+    let token = jwt.sign({ _id: this._id },
+      process.env.SECRET_KEY
+    );
+    this.tokens = this.tokens.concat({ token: token });
+    await this.save();
+    return token;
+  } catch (error) {
+    console.log(err);
+  }
+}
 
 const User = mongoose.model("USER", userSchema);
 module.exports = User;
